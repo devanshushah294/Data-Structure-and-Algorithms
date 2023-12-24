23686b7af610337098b662b2f1b88634161dc0f2
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-12-24 21:45:39 +0530

    Create BinaryTreesB.java

---
+++ BinaryTrees/BinaryTreesB.java
@@ -0,0 +1,99 @@
+// import BinaryTreesB.Node.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreesB {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        public static void preOrder(Node root) {
+            // Time Complexity O(n)
+            Node temp = root;
+            if (root == null) {
+                // System.out.print("-1" + " ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+
+        }
+
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public static void levelOrder(Node root) {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            queue.add(null);
+            while (!queue.isEmpty()) {
+                Node currNode = queue.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    queue.add(null);
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        queue.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        queue.add(currNode.right);
+                    }
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree b = new BinaryTree();
+        Node root = b.buildTree(nodes);
+        System.out.println(root.data);
+        b.preOrder(root);
+        System.out.println("");
+        b.inOrder(root);
+        System.out.println("");
+        b.postOrder(root);
+        // b.levelOrder(root);
+    }
+}
